ee3642378ccfb3cf664e752a029c8433de4f75e4
János Veres <janos@dot.industries>
2023-05-01 18:00:35 +0200

    feat: simple worker pool

---
+++ apps/api/worker/worker.go
@@ -0,0 +1,27 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+func worker(ctx context.Context, wg *sync.WaitGroup, jobs <-chan Job, results chan<- Result) {
+	defer wg.Done()
+	for {
+		select {
+		case job, ok := <-jobs:
+			if !ok {
+				return
+			}
+			// fan-in job execution multiplexing results into the results channel
+			results <- job.execute(ctx)
+		case <-ctx.Done():
+			fmt.Printf("cancelled worker. Error detail: %v\n", ctx.Err())
+			results <- Result{
+				Err: ctx.Err(),
+			}
+			return
+		}
+	}
+}

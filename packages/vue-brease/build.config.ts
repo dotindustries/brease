0bb0501220547e0e3f768388329e116456b8f082
János Veres <janos@dot.industries>
2023-06-14 12:13:28 +0200

    build: move from rollup to unbuild

---
+++ packages/vue-brease/build.config.ts
@@ -0,0 +1,15 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  // If entries is not provided, will be automatically inferred from package.json
+  entries: [
+    // default
+    "./src/index",
+  ],
+
+  // Change outDir, default is 'dist'
+  outDir: "build",
+
+  // Generates .d.ts declaration file
+  declaration: true,
+});

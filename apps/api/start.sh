2e424d2f916072a518b6598141660e97e8e0e067
János Veres <janos@dot.industries>
2025-07-24 22:47:23 +0200

    fix: export env var for infisical run

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Fetching secrets from Infisical..."
-INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_CLIENT_ID --client-secret=$INFISICAL_CLIENT_SECRET --plain --silent)
+export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_CLIENT_ID --client-secret=$INFISICAL_CLIENT_SECRET --plain --silent)
 infisical run --projectId=$INFISICAL_PROJECT_ID --env=staging brease
